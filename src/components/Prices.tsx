a71ef79da5c4127f3e148b4d43833730b4922c3b
Kevin Haffner <khaffner@gmail.com>
2021-11-05 20:03:06 +0100

    Change env var prefix

--- src/components/Prices.tsx
+++ src/components/Prices.tsx
@@ -8,16 +8,17 @@ const entsoeTimeFormat = 'yyyymmddHH00'
 const start = dateFormat(now, entsoeTimeFormat)
 const end = dateFormat(now.setHours(now.getHours() + 12), entsoeTimeFormat)
 
-const token = process.env.ENTSOE_TOKEN;
-const url =
-    'https://transparency.entsoe.eu/api?documentType=A44&in_Domain=10YNO-1--------2&out_Domain=10YNO-1--------2&periodStart=' +
-    start +
-    '&periodEnd=' +
-    end +
-    '&securityToken=' +
-    token
-
 const Prices = () => {
+    const token = process.env.REACT_APP_ENTSOE_TOKEN
+    console.log(token)
+    const url =
+        'https://transparency.entsoe.eu/api?documentType=A44&in_Domain=10YNO-1--------2&out_Domain=10YNO-1--------2&periodStart=' +
+        start +
+        '&periodEnd=' +
+        end +
+        '&securityToken=' +
+        token
+
     const [response, setResponse] = React.useState('')
 
     useEffect(() => {
